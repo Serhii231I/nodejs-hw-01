5003cb01fce8f8aeac1b8cb484dfce4943167ff9
Serhii Ivashchenko <Ivashchenkoserezha25@gmail.com>
2025-03-08 19:12:38 +0100

    fix

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,19 +1,17 @@
-import { createFakeContact } from "../utils/createFakeContact.js";
-import { readContacts } from "../utils/readContacts.js";
-import { writeContacts } from "../utils/writeContacts.js";
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
 const generateContacts = async (number) => {
-    try {
-      const contactsList = await  readContacts();
-        for (let i = 1; i <= number; i++){
-            contactsList.push(createFakeContact());
-        }
-        await writeContacts(contactsList);
-        }
-     catch (error) {
-        console.log(error.message);
-
+  try {
+    const contactsList = await readContacts();
+    for (let i = 1; i <= number; i++) {
+      contactsList.push(createFakeContact());
     }
+    await writeContacts(contactsList);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 generateContacts(5);
